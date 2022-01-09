5822c5e75dd583a9dc6e1364b01c729aeefa90d6
otsuka <slowird7@gmail.com>
2022-01-09 20:30:05 +0900

    multi tank random walk.

--- src/main/java/com/app/ahonokotank/FXMLController.java
+++ src/main/java/com/app/ahonokotank/FXMLController.java
@@ -6,6 +6,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.TimerTask;
+import java.util.Timer;
+import javafx.application.Platform;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,7 +18,9 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.text.Font;
 
 public class FXMLController implements Initializable {
@@ -21,6 +28,7 @@ public class FXMLController implements Initializable {
     public static final int mapSize = 10;
 
     public static FXMLController INSTANCE;
+    private static int NO_OF_TANKS = 4;
 
     public enum DIRECTION {
         NORTH(0),
@@ -71,47 +79,82 @@ public class FXMLController implements Initializable {
     public int columns = 0, rows = 0;
 
     @FXML
-    private Button btnStart;
+    private Button btnRun;
     @FXML
     private GridPane theMap;
 
     private Label cell[][];
     private char map[][];
 
-    private Tank tank1;
+    private Tank[] tank;
+    private BooleanProperty isRunning;
+
+    private Timer timer;
 
     @FXML
     private void onBtnStart(ActionEvent event) {
-        tank1.init();
-        plotTank(tank1);
-        theMap.getScene().getWindow().showingProperty().addListener((observable, oldValue, newValue) -> {
-            if (oldValue == true && newValue == false) {
-                tank1.terminate();
+        if (!isRunning.get()) {
+            for (int nn = 0; nn < NO_OF_TANKS; nn++) {
+                tank[nn] = new Tank();
+                tank[nn].init();
+                plotTank(tank[nn]);
             }
-        });
-        tank1.startController();
+            btnRun.setText("stop");
+            timer = new Timer(false);
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    for (int nn = 0; nn < NO_OF_TANKS; nn++) {
+                        tank[nn].run();
+                    }
+                }
+            };
+
+            timer.scheduleAtFixedRate(task, 0, 500);
+        } else {
+            timer.cancel();
+            btnRun.setText("start");
+        }
+        isRunning.set(!isRunning.get());
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         loadMap("map.txt");
-        tank1 = new Tank();
+        tank = new Tank[NO_OF_TANKS];
         INSTANCE = this;
+        isRunning = new SimpleBooleanProperty(false);
+        Platform.runLater(() -> {
+            theMap.getScene().getWindow().showingProperty().addListener((observable, oldValue, newValue) -> {
+                if (oldValue == true && newValue == false) {
+                    timer.cancel();
+                }
+            });
+        });
+    }
 
+    private BooleanProperty isRunningProperty() {
+        return isRunning;
     }
 
     private void loadMap(String mapName) {
+        theMap.getRowConstraints().clear();
+        theMap.getColumnConstraints().clear();
         File file = new File(mapName);
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = br.readLine()) != null) {
                 rows++;
+                theMap.getRowConstraints().add(new RowConstraints(30));
                 if (columns < line.length()) {
                     columns = line.length();
                 }
             }
             cell = new Label[rows][columns];
             map = new char[rows][columns];
+            for (int xx = 0; xx < columns; xx++) {
+                theMap.getColumnConstraints().add(new ColumnConstraints(30));
+            }
         } catch (IOException ex) {
             new Alert(Alert.AlertType.WARNING, "map file not found. [" + mapName + "]", ButtonType.OK).showAndWait();
         }
@@ -126,9 +169,9 @@ public class FXMLController implements Initializable {
                     cell[yy][xx].setVisible(true);
                     cell[yy][xx].setFont(new Font("system", 22));
                     cell[yy][xx].setEllipsisString("");
+                    theMap.getChildren().add(cell[yy][xx]);
                     theMap.setRowIndex(cell[yy][xx], yy);
                     theMap.setColumnIndex(cell[yy][xx], xx);
-                    theMap.getChildren().add(cell[yy][xx]);
                 }
             }
 
@@ -193,6 +236,195 @@ public class FXMLController implements Initializable {
             default:
                 throw new IllegalArgumentException("unknown direction:" + tank.td);
         }
+    }
+
+//======================================================================
+    public boolean isForwardOK(Tank tank) {
+        Tank newPos = new Tank(tank);
+        switch (tank.td) {
+            case NORTH:
+                newPos.ty--;
+                break;
+            case EAST:
+                newPos.tx++;
+                break;
+            case SOUTH:
+                newPos.ty++;
+                break;
+            case WEST:
+                newPos.tx--;
+                break;
+        }
+        return isLocateOK(newPos);
+    }
+
+    public boolean isBackwardOK(Tank tank) {
+        Tank newPos = new Tank(tank);
+        switch (tank.td) {
+            case NORTH:
+                newPos.ty++;
+                break;
+            case EAST:
+                newPos.tx--;
+                break;
+            case SOUTH:
+                newPos.ty--;
+                break;
+            case WEST:
+                newPos.tx++;
+                break;
+        }
+        return isLocateOK(newPos);
+    }
+
+    public boolean isTurnLeftOK(Tank tank) {
+        Tank newPos = new Tank(tank);
+        switch (tank.td) {
+            case NORTH:
+                newPos.td = DIRECTION.WEST;
+                break;
+            case EAST:
+                newPos.td = DIRECTION.NORTH;
+                break;
+            case SOUTH:
+                newPos.td = DIRECTION.EAST;
+                break;
+            case WEST:
+                newPos.td = DIRECTION.SOUTH;
+                break;
+        }
+        return isLocateOK(newPos);
+    }
+
+    public boolean isTurnRightOK(Tank tank) {
+        Tank newPos = new Tank(tank);
+        switch (tank.td) {
+            case NORTH:
+                newPos.td = DIRECTION.EAST;
+                break;
+            case EAST:
+                newPos.td = DIRECTION.SOUTH;
+                break;
+            case SOUTH:
+                newPos.td = DIRECTION.WEST;
+                break;
+            case WEST:
+                newPos.td = DIRECTION.NORTH;
+                break;
+        }
+        return isLocateOK(newPos);
+    }
+
+    public boolean moveForward(Tank tank) {
+        if (!isForwardOK(tank)) {
+            return false;
+        }
+        Tank current = new Tank(tank);
+        Platform.runLater(() -> {
+            FXMLController.INSTANCE.clearTank(current);
+        });
+        switch (tank.td) {
+            case NORTH:
+                tank.ty--;
+                break;
+            case EAST:
+                tank.tx++;
+                break;
+            case SOUTH:
+                tank.ty++;
+                break;
+            case WEST:
+                tank.tx--;
+                break;
+        }
+        Platform.runLater(() -> {
+            FXMLController.INSTANCE.plotTank(tank);
+        });
+        return true;
+    }
+
+    public boolean moveBackward(Tank tank) {
+        if (!isBackwardOK(tank)) {
+            return false;
+        }
+        Tank current = new Tank(tank);
+        Platform.runLater(() -> {
+            FXMLController.INSTANCE.clearTank(current);
+        });
+        switch (tank.td) {
+            case NORTH:
+                tank.ty++;
+                break;
+            case EAST:
+                tank.tx--;
+                break;
+            case SOUTH:
+                tank.ty--;
+                break;
+            case WEST:
+                tank.tx++;
+                break;
+        }
+        Platform.runLater(() -> {
+            FXMLController.INSTANCE.plotTank(tank);
+        });
+        return true;
+    }
+
+    public boolean turnLeft(Tank tank) {
+        if (!isTurnLeftOK(tank)) {
+            return false;
+        }
+        Tank current = new Tank(tank);
+        Platform.runLater(() -> {
+            FXMLController.INSTANCE.clearTank(current);
+        });
+        switch (tank.td) {
+            case NORTH:
+                tank.td = DIRECTION.WEST;
+                break;
+            case EAST:
+                tank.td = DIRECTION.NORTH;
+                break;
+            case SOUTH:
+                tank.td = DIRECTION.EAST;
+                break;
+            case WEST:
+                tank.td = DIRECTION.SOUTH;
+                break;
+        }
+        Platform.runLater(() -> {
+            FXMLController.INSTANCE.plotTank(tank);
+        });
+        return true;
+    }
+
+    public boolean turnRight(Tank tank) {
+        if (!isTurnRightOK(tank)) {
+            return false;
+        }
+        Tank current = new Tank(tank);
+        Platform.runLater(() -> {
+            FXMLController.INSTANCE.clearTank(current);
+        });
+        switch (tank.td) {
+            case NORTH:
+                tank.td = DIRECTION.EAST;
+                break;
+            case EAST:
+                tank.td = DIRECTION.SOUTH;
+                break;
+            case SOUTH:
+                tank.td = DIRECTION.WEST;
+                break;
+            case WEST:
+                tank.td = DIRECTION.NORTH;
+                break;
+        }
+        Platform.runLater(() -> {
+            FXMLController.INSTANCE.plotTank(tank);
+        });
+        return true;
     }
 
 }
--- src/main/java/com/app/ahonokotank/Tank.java
+++ src/main/java/com/app/ahonokotank/Tank.java
@@ -8,11 +8,10 @@ package com.app.ahonokotank;
 import com.app.ahonokotank.FXMLController.DIRECTION;
 import static com.app.ahonokotank.User32.INSTANCE;
 import java.util.Random;
-import java.util.TimerTask;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
-import java.util.Timer;
 import javafx.application.Platform;
+
 /**
  *
  * @author otsuka
@@ -24,18 +23,17 @@ public class Tank {
     public DIRECTION td, md;
     public boolean inFight = false;
     private Random random;
-    Timer timer;
-    
+
     public Tank() {
-        
+
     }
-    
-    private Tank(Tank tank) {
+
+    public Tank(Tank tank) {
         this.tx = tank.tx;
         this.ty = tank.ty;
         this.td = tank.td;
     }
-    
+
     void init() {
         random = new Random();
         td = DIRECTION.NORTH;
@@ -43,157 +41,83 @@ public class Tank {
             tx = random.nextInt(FXMLController.INSTANCE.columns);
             ty = random.nextInt(FXMLController.INSTANCE.rows);
             switch (random.nextInt(3)) {
-                case 0: td = DIRECTION.NORTH; break;
-                case 1: td = DIRECTION.EAST; break;
-                case 2: td = DIRECTION.SOUTH; break;
-                case 3: td = DIRECTION.WEST; break;
+                case 0:
+                    td = DIRECTION.NORTH;
+                    break;
+                case 1:
+                    td = DIRECTION.EAST;
+                    break;
+                case 2:
+                    td = DIRECTION.SOUTH;
+                    break;
+                case 3:
+                    td = DIRECTION.WEST;
+                    break;
             }
         }
     }
 
     boolean isKeyPressed(short key) {
         return (INSTANCE.GetKeyState(key) & 0x8000) == 0x8000;
-    };
+    }
+
+    ;
 
     public boolean isForwardOK() {
-        Tank newPos = new Tank(this);
-        switch(td) {
-            case NORTH: newPos.ty--;break;
-            case EAST : newPos.tx++;break;
-            case SOUTH: newPos.ty++;break;
-            case WEST : newPos.tx--;break;
-        }
-        return FXMLController.INSTANCE.isLocateOK(newPos);
+        return FXMLController.INSTANCE.isForwardOK(this);
     }
-    
+
     public boolean isBackwardOK() {
-        Tank newPos = new Tank(this);
-        switch(td) {
-            case NORTH: newPos.ty++; break;
-            case EAST : newPos.tx--; break;
-            case SOUTH: newPos.ty--; break;
-            case WEST : newPos.tx++; break;
-        }
-        return FXMLController.INSTANCE.isLocateOK(newPos);
+        return FXMLController.INSTANCE.isBackwardOK(this);
     }
-    
+
     public boolean isTurnLeftOK() {
-        Tank newPos = new Tank(this);
-        switch(td) {
-            case NORTH: newPos.td = DIRECTION.WEST;  break;
-            case EAST : newPos.td = DIRECTION.NORTH; break;
-            case SOUTH: newPos.td = DIRECTION.EAST;  break;
-            case WEST : newPos.td = DIRECTION.SOUTH; break;
-        }
-        return FXMLController.INSTANCE.isLocateOK(newPos);
+        return FXMLController.INSTANCE.isTurnLeftOK(this);
     }
-    
+
     public boolean isTurnRightOK() {
-        Tank newPos = new Tank(this);
-        switch(td) {
-            case NORTH: newPos.td = DIRECTION.EAST;  break;
-            case EAST : newPos.td = DIRECTION.SOUTH; break;
-            case SOUTH: newPos.td = DIRECTION.WEST;  break;
-            case WEST : newPos.td = DIRECTION.NORTH; break;
-        }
-        return FXMLController.INSTANCE.isLocateOK(newPos);
+        return FXMLController.INSTANCE.isTurnRightOK(this);
     }
-    
+
     public void clear() {
-        
+
     }
 
-    public void moveForward() {
-        Tank current = new Tank(this);
-        Platform.runLater(()->{
-            FXMLController.INSTANCE.clearTank(current);
-        });
-        switch(td) {
-            case NORTH: this.ty--;break;
-            case EAST : this.tx++;break;
-            case SOUTH: this.ty++;break;
-            case WEST : this.tx--;break;
-        }
-        Platform.runLater(()->{
-            FXMLController.INSTANCE.plotTank(this);
-        });
+    public boolean moveForward() {
+        return FXMLController.INSTANCE.moveForward(this);
     }
-    
-    public void moveBackward() {
-        Tank current = new Tank(this);
-        Platform.runLater(()->{
-            FXMLController.INSTANCE.clearTank(current);
-        });
-        switch(td) {
-            case NORTH: this.ty++;break;
-            case EAST : this.tx--;break;
-            case SOUTH: this.ty--;break;
-            case WEST : this.tx++;break;
-        }
-        Platform.runLater(()->{
-            FXMLController.INSTANCE.plotTank(this);
-        });
-    }
-    
-    public void turnLeft() {
-        Tank current = new Tank(this);
-        Platform.runLater(()->{
-            FXMLController.INSTANCE.clearTank(current);
-        });
-        switch(td) {
-            case NORTH: this.td = DIRECTION.WEST;  break;
-            case EAST : this.td = DIRECTION.NORTH; break;
-            case SOUTH: this.td = DIRECTION.EAST;  break;
-            case WEST : this.td = DIRECTION.SOUTH; break;
-        }
-        Platform.runLater(()->{
-            FXMLController.INSTANCE.plotTank(this);
-        });
-    }
-    
-    public void turnRight() {
-        Tank current = new Tank(this);
-        Platform.runLater(()->{
-            FXMLController.INSTANCE.clearTank(current);
-        });
-        switch(td) {
-            case NORTH: this.td = DIRECTION.EAST;  break;
-            case EAST : this.td = DIRECTION.SOUTH; break;
-            case SOUTH: this.td = DIRECTION.WEST;  break;
-            case WEST : this.td = DIRECTION.NORTH; break;
-        }
-        Platform.runLater(()->{
-            FXMLController.INSTANCE.plotTank(this);
-        });
-    }
-    
-    void startController() {
-        timer = new Timer(false);
-        TimerTask task = new TimerTask() {
-            long beforeTime = System.currentTimeMillis();
-            int cnt = 0;
 
-            @Override
-            public void run() {
-                clear();
-                int x = random.nextInt(100);
-                if(x < 50) {
-                    if (isForwardOK()) moveForward();
-                } else if (x < 60) {
-                    if (isBackwardOK()) moveBackward();
-                } else if (x < 80) {
-                    if(isTurnLeftOK()) turnLeft();
-                } else {
-                    if (isTurnRightOK()) turnRight();
-                }
+    public boolean moveBackward() {
+        return FXMLController.INSTANCE.moveBackward(this);
+    }
+
+    public boolean turnLeft() {
+        return FXMLController.INSTANCE.turnLeft(this);
+    }
+
+    public boolean turnRight() {
+        return FXMLController.INSTANCE.turnRight(this);
+    }
+
+    void run() {
+        while (true) {
+            int x = random.nextInt(100);
+            if (x < 70 && isForwardOK()) {
+                moveForward();
+                break;
+            } else if (x < 80 && isTurnLeftOK()) {
+                turnLeft();
+                break;
+
+            } else if (x < 90 && isTurnRightOK()) {
+                turnRight();
+                break;
+
+            } else if (isBackwardOK()) {
+                moveBackward();
+                break;
             }
-        };
-        
-        timer.scheduleAtFixedRate(task, 0, 500);
-    }
-    
-    public void terminate() {
-        timer.cancel();
+        }
     }
 
 }
